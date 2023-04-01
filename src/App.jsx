0eb1c034480f785c9fb4fb3ecb803b886bcbf572
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-03-31 20:34:03 -0400

    Added styling for Balance section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+function Balance() {
+  return (
+    <div className="balance-container">
+        <div className="balance-values">
+          <h2>My balance</h2>
+          <h1>$921.48</h1>
+        </div>
+        <img src="../images/logo.svg" alt="logo"></img>
+    </div>
+  )
+}
 
+function Spending() {
+
+}
 
 function App() {
-  return <h1>Hello there</h1>
+  return (
+    <div>
+      <Balance />
+      <Spending />
+    </div>
+  )
 }
 
 export default App
