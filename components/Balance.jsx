646a7e09e31a051a6d5456291ee8755ef3786719
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-04-01 13:34:18 -0400

    Added public folder

--- components/Balance.jsx
+++ components/Balance.jsx
@@ -7,7 +7,7 @@ function Balance() {
             <h3>My balance</h3>
             <h1>$921.48</h1>
           </div>
-          <img src="../images/logo.svg" alt="logo"></img>
+          <img src="/images/logo.svg" alt="logo"></img>
       </section>
     )
   }
