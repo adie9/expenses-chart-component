6f15bf4bb4635cfcc552053f8af4a1c41fd087cf
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-04-01 13:30:43 -0400

    minor changes

--- components/Spending.jsx
+++ components/Spending.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react"
 import Chart from "../components/Chart"
 import Data from "../data.json"
 
+
+
 function Spending() {
-  const [userData, setData] = useState({
+  const [userData, setUserData] = useState({
     labels: ['monday', 'tuesday', 'wednesday', 'thursday',
                 'friday', 'saturday', 'sunday'],
     datasets: [
         {
+            label: '$',
             data: [25, 55, 40, 30, 20, 35, 10],
             backgroundColor: ['rgba(255, 0, 0, 0.5)'],
             borderWidth: 1,
@@ -15,23 +18,35 @@ function Spending() {
         }
     ]
   })
+
+  // const [Data, setData] = useState({
+  //   labels: Data.map((data) => data.day),
+  //   datasets: [{
+  //     label: "$",
+  //     data: Data.map((data) => data.amount),
+  //     backgroundColor: ['rgba(255, 0, 0, 0.5)'],
+  //           borderWidth: 1,
+  //           color: '#eb755d'
+  //   }]
+  // })
+
+
   
   
 
-
-    return (
-      <section className="spending-container">
-        <h1>Spending - Last 7 Days</h1>
-        <Chart chartData={userData}  />
-        <div class="spending-footer">
-            <div class="spending-values">
-                <h3>Total this month: </h3>
-                <h1>$478.33</h1>
-            </div>
-            <h3 id="percent">+2.4% <br></br><span>from last month</span></h3>
-        </div>
-      </section>
-    )
-  }
+  return (
+    <section className="spending-container">
+      <h1>Spending - Last 7 Days</h1>
+      <Chart chartData={userData}  />
+      <div class="spending-footer">
+          <div class="spending-values">
+              <h3>Total this month: </h3>
+              <h1>$478.33</h1>
+          </div>
+          <h3 id="percent">+2.4% <br></br><span>from last month</span></h3>
+      </div>
+    </section>
+  )
+}
 
   export default Spending
