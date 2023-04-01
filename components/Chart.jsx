e35dba3b36959fb4d975572301fc6f8272c8fc96
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-04-01 17:50:39 -0400

    dynamically change total based on data

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -3,8 +3,6 @@ import { Bar } from "react-chartjs-2"
 import {Chart as ChartJS} from 'chart.js/auto'
 
 function Chart({chartData}) {
-  console.log(chartData)
-
   return (
       <div id="chart">
         <Bar data={chartData} />
--- components/Spending.jsx
+++ components/Spending.jsx
@@ -6,6 +6,10 @@ import Chart from "../components/Chart"
 
 function Spending({chartData}) {
 
+  const amounts = chartData.datasets[0].data
+  const total = amounts.reduce((total, num) => total + num, 0)
+  
+
   return (
     <section className="spending-container">
       <h1>Spending - Last 7 Days</h1>
@@ -13,7 +17,7 @@ function Spending({chartData}) {
       <div class="spending-footer">
           <div class="spending-values">
               <h3>Total this month: </h3>
-              <h1>$478.33</h1>
+              <h1>${total}</h1>
           </div>
           <h3 id="percent">+2.4% <br></br><span>from last month</span></h3>
       </div>
